1a26be9060073fe9ba3660450f83ca2de6b1c2ea
Atong Abraham Kur <atongkurabraham@gmail.com>
2024-07-22 15:21:10 +0300

    Updated the home page

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
+// main.js
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+
+// Import Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+// Import Bootstrap JavaScript
+import 'bootstrap';
 
 const app = createApp(App);
 
